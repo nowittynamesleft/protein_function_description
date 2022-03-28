Fixing subsampling and changed robustness to be averaged over only sets that have more than one correct term assigned

data.py
@@ -148,17 +148,11 @@ class SequenceGOCSVDataset(Dataset):
         union_go_term_mask = np.any(self.go_annot_mat[annot_inds], axis=0)
         return union_go_term_mask
 
-    '''
-    def get_all_valid_terms(self, prot_ids):
-        valid_term_inds = self.check_prot_set_annots(prot_ids)
-        valid_go_terms = self.go_terms[valid_term_inds]
-        if len(valid_go_terms) > 1:
-            print('More than one valid go term ind!')
-            print(valid_go_terms)
-
-        return valid_go_terms
-    '''
     def get_identically_annotated_subsamples(self, go_term_index, num_sets):
+        try:
+            assert num_sets > 1
+        except AssertionError:
+            print('num_sets to subsample must be greater than 1.')
         # get multiple subsets of proteins that have the same GO terms in common within each set of proteins, given a single go term that you want the sets to have
         annotated_seqs = self.get_annotated_seqs(go_term_index)[0]
         annotated_prot_ids = self.get_annotated_prot_ids(go_term_index)[0] # only consider proteins containing the go term specified
@@ -181,8 +175,8 @@ class SequenceGOCSVDataset(Dataset):
             counter += 1
             if counter > 1000:
                 import ipdb; ipdb.set_trace()
-        print('Number of tries to get an appropriate protein set to choose ' + str(num_sets) + ':' + str(counter))
-        print(common_terms)
+        #print('Number of tries to get an appropriate protein set to choose ' + str(num_sets) + ':' + str(counter))
+        #print(common_terms)
         '''
         # once there are enough protein sets to possibly sample, check what other terms the proteins have
         all_term_mask = self.get_union_of_terms(prots_with_terms)
@@ -244,12 +238,12 @@ class SequenceGOCSVDataset(Dataset):
             print('Not all sets chosen have common terms chosen!')
             import ipdb; ipdb.set_trace()
 
-        print('Length of protein sets chosen:')
-        print([len(prot_set) for prot_set in prot_id_sets])
-        print('Rejected sampled sets: ' + str(rejected))
-        print('Total tries: ' + str(total_tries))
-        print('Rejection rate:')
-        print(rejected/total_tries)
+        #print('Length of protein sets chosen:')
+        #print([len(prot_set) for prot_set in prot_id_sets])
+        #print('Rejected sampled sets: ' + str(rejected))
+        #print('Total tries: ' + str(total_tries))
+        #print('Rejection rate:')
+        #print(rejected/total_tries)
         
         return (prot_id_sets, seq_sets, common_terms, rejected, total_tries)
         
@@ -417,24 +411,26 @@ def create_annot_mat(prot_ids, go_terms, go2prot_ids):
     return annot_mat, prot_id2annot_ind
 
 
-def test_identical_subsampling(test_csv):
-    dataset = SequenceGOCSVDataset(test_csv, 'go.obo', 1)
+def test_identical_subsampling(test_csv, num_sets, seq_set_len):
+    dataset = SequenceGOCSVDataset(test_csv, 'go.obo', seq_set_len)
     print('loaded dataset')
-    num_sets = 2 # do I need a num_sets parameter as long as I have at least two sets to compute the metric?? I can always increase the number of sets for a given GO term instead I guess...
     rejection_rate = 0
     num_excluded_terms = 0
+    common_terms_lists = []
     for term in tqdm.tqdm(range(0, len(dataset.go_terms))):
         try:
             (prot_id_sets, seq_sets, common_terms, rejected, total_tries) = dataset.get_identically_annotated_subsamples(term, num_sets)
             rejection_rate += rejected/total_tries
+            common_terms_lists.append(common_terms)
         except AssertionError:
             num_excluded_terms += 1
             pass
     rejection_rate /= (len(dataset.go_terms) - num_excluded_terms)
     print('Rejection rate (average): ' + str(rejection_rate))
+    avg_num_common_terms = sum([len(term_list) for term_list in common_terms_lists])/len(common_terms_lists)
+    print('Average number of common terms: ' + str(avg_num_common_terms))
     #import ipdb; ipdb.set_trace()
     
 
 if __name__ == '__main__':
-    #test_identical_subsampling('first_100_uniprot_training_annot.csv')
-    test_identical_subsampling('uniprot_sprot_training_val_split.csv')
+    test_identical_subsampling('uniprot_sprot_training_val_split.csv', 2, 16)

train_and_test_model.py
@@ -246,9 +246,10 @@ def classification(model, dataset, save_prefix='no_prefix', num_subsamples=10):
     print(str(len(included_go_inds)) + "-way zero-shot classification.")
     ground_truth = []
     print("Num subsamples per term:" + str(num_subsamples))
+    print('Getting identically annotated subsamples for robustness calculation.')
     for ind in tqdm.tqdm(included_go_inds):
         #(prot_id_sets, seq_sets) = zip(*[dataset[ind] for it in range(num_subsamples)])
-        (prot_id_sets, seq_sets) = dataset.get_identically_annotated_subsamples(ind, num_subsamples)
+        (prot_id_sets, seq_sets, _, _, _) = dataset.get_identically_annotated_subsamples(ind, num_subsamples)
         valid_term_mask = [dataset.get_all_valid_term_mask(prot_id_set) for prot_id_set in prot_id_sets]
         ground_truth.extend(valid_term_mask)
         S_padded, S_mask = seq_go_collate_pad(list(zip(prot_id_sets, seq_sets)), seq_set_size=len(seq_sets[0])) # batch sizes of 1 each, index out of it
@@ -262,18 +263,10 @@ def classification(model, dataset, save_prefix='no_prefix', num_subsamples=10):
     print("Mean reciprocal rank")
     mrr = mean_reciprocal_rank(preds, ground_truth)
     print(mrr)
-    
-    '''
-    if preds.shape[1] < 1000:
-        accs = accuracy(preds, ground_truth, topk=(5, 4, 3, 2, 1))
-        print('Len penalty: Top 5, 4, 3, 2, 1 accuracies: ' + str(accs))
-    else:
-        accs = accuracy(preds, ground_truth, topk=(1000, 500, 100, 50, 10, 5, 1))
-        print('Len penalty: Top 1000, 500, 100, 50, 10, 5, 1 accuracies: ' + str(accs))
-    '''
 
     return preds
     
+
 
 def mean_reciprocal_rank(preds, ground_truth):
     rankings = torch.argsort(torch.sort(preds, dim=1, descending=True)[1], dim=1) + 1

utils.py
@@ -298,6 +298,7 @@ def annotation_robustness(log_prob_mat, n, correct_go_inds):
     assert all_subsamples_have_same_annots(torch.stack([torch.tensor(row) for row in correct_go_inds]), n)
     all_corr = 0
     num_go_term_sets = int(log_prob_mat.shape[0]/n)
+    num_included_terms = 0
     for go_term_set_ind in range(num_go_term_sets):
         rows = log_prob_mat[go_term_set_ind*n:go_term_set_ind*n + n, :]
         correct_mask = torch.zeros_like(rows[0, :], dtype=bool)
@@ -316,7 +317,8 @@ def annotation_robustness(log_prob_mat, n, correct_go_inds):
                         total_correlation += spearman_correlation(score_vec, score_vec_2)
             curr_avg_corr = total_correlation/(n*(n-1))
             all_corr += curr_avg_corr
-    all_corr /= num_go_term_sets
+            num_included_terms += 1
+    all_corr /= num_included_terms
     return all_corr
 
 